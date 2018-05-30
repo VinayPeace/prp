2773c7f0d89327e4a545bf33168579a6b8bc26b3
VinayPeace <vinayr173@gmail.com>
2018-05-30 15:15:57 +0530

    hello

---
+++ basics/src/basics/Sampleprogram.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class Sampleprogram {
+	public static void main(String args[]){
+	int x = 1, total=1;
+	while ( x <= 10 )
+	{
+	total += x;
+	++x;
+	System.out.println(total);
+	}
+	}
+}
+
+	
+	
+	
