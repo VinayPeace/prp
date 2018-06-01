7dfc740018c3478a86f4c24e6c5146acdb95b82e
VinayPeace <vinayr173@gmail.com>
2018-06-01 16:08:49 +0530

    arrayreverse

---
+++ basics/src/basics/Array.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class Array {
+	public static void main(String[] args) {
+
+        
+       int n=1234;
+       int number[]=new int[10];
+       for(int i=0;i<number.length;i++)
+       {
+    	   number[i]=n%10;
+    	   n/=10;
+    	   System.out.println(number[i]);
+        }
+    }
+}
+
+
