9cca8e13bc9ee9904d6d633ce369280fa25dd061
VinayPeace <vinayr173@gmail.com>
2018-06-03 15:42:56 +0530

    profile

---
+++ MyProfile/src/com/personal/VinayviratProfile.java
@@ -0,0 +1,21 @@
+package com.personal;
+
+public class  VinayviratProfile implements IProfile{
+
+	@Override
+	public void myBasicInfo() {
+		System.out.println("myBasicInfo : Vinay");
+		System.out.println("myBasicInfo : ECE");
+		
+	}
+    @Override
+	public void myHobbies() {
+    	System.out.println("myHobbies : Cricket");
+	
+	}
+
+	
+	}
+	
+
+
