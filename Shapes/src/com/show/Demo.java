720fdec7648406d4addcad6d66d92965388a1621
VinayPeace <vinayr173@gmail.com>
2018-06-06 12:27:33 +0530

    Shapes

---
+++ Shapes/src/com/show/Demo.java
@@ -0,0 +1,20 @@
+package com.show;
+
+public class Demo {
+	public static void main(String args[])
+	{
+		TwoDimensional t1=new TwoDimensional();
+		ThreeDimensional t2=new ThreeDimensional();
+		Sphere t3=new Sphere();
+		Quadrilateral q=new Quadrilateral();
+		t1.Square();
+		t1.Rectangle();
+		t1.Circle();
+		t1.Triangle();
+		t2.Cube();
+		t2.Cylinder();
+		t3.Spherearea();
+	q.show();
+
+}
+}
---
+++ Shapes/src/com/show/Shape.java
@@ -0,0 +1,9 @@
+package com.show;
+
+public class Shape {
+	public double length=30.0;
+	public double width=10.0;
+	public double radius=5.0;
+public double area;
+
+}
