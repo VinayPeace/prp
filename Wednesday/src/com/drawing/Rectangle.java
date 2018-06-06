f2433de0e2b0e7f301a201f53b6afc46b0627156
VinayPeace <vinayr173@gmail.com>
2018-06-06 10:52:30 +0530

    Rectangle

---
+++ Wednesday/src/com/drawing/Rectangle.java
@@ -0,0 +1,57 @@
+package com.drawing;
+
+public class Rectangle {
+	double length;
+	double width;
+	public Rectangle()
+	{
+		
+	}
+	public double getLength() {
+		return length;
+	}
+	public double setLength(double length) {
+		if(length>0.0&&length<20.0)
+		{
+		this.length = length;
+		return length;
+		}
+		else
+		{
+			return 0.0;
+		}
+	}
+	public double getWidth() {
+		return width;
+	}
+	public double setWidth(double width) {
+		if(width>0.0&&width<20.0)
+		{
+		this.width = width;
+		return width;
+		}
+		else
+		{
+		return 0.0;
+		}
+	}
+	public double area()
+	{
+		double a=length*width;
+		return a;
+	}
+	public double perimeter()
+	{
+		double p=(length+width)*2;
+		return p;
+	}
+	public static void main(String a[])
+	{
+		Rectangle r=new Rectangle();
+		r.setLength(5.0);
+		r.setWidth(10.0);
+		System.out.println("area="+r.area());
+		System.out.println("perimeter="+r.perimeter());
+
+}
+}
